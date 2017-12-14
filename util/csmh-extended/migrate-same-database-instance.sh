a47a2892059a1eac2a91d92dc9718c9e953a18c1
Your Name <you@example.com>
2017-12-14 13:15:17 +0000

    Fix db migrate script

--- util/csmh-extended/migrate-same-database-instance.sh
+++ util/csmh-extended/migrate-same-database-instance.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 MINID=0
-MAXID=1003426362
+MAXID=300000
 STEP=10000
 MIGRATE_USER=migrate
 PASSWORD='secret-password'
@@ -8,10 +9,10 @@ HOST='my-database-instance'
  
 for ((i=0; i<=$MAXID; i+=$STEP)); do
 echo -n "$i";
-mysql -u $MIGRATE_USER -p$PASSWORD -h $HOST wwc <<EOF
+mysql edxapp <<EOF
 INSERT INTO edxapp_csmh.coursewarehistoryextended_studentmodulehistoryextended (id, version, created, state, grade, max_grade, student_module_id)
   SELECT id, version, created, state, grade, max_grade, student_module_id
-  FROM wwc.courseware_studentmodulehistory
+  FROM edxapp.courseware_studentmodulehistory
   WHERE id BETWEEN $i AND $(($i+$STEP-1));
 EOF
 echo '.';
