8f0d8e5b16bc749b3f3a5df81c43683aa94881e2
Dmitry Gamanenko <dgamanenko@gmail.com>
2017-12-22 10:00:36 +0000

    Cleanup after migrate to single AWS instance.

--- update_certs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CONFIGURATION_VENV_BIN_PATH='/edx/app/edx_ansible/venvs/edx_ansible/bin'
-CONFIGURATION_REPO_PATH='/edx/app/edx_ansible/edx_ansible'
-SERVER_VARS='/edx/app/edx_ansible/server-vars.yml'
-HOSTS_FILE='/edx/app/edx_ansible/hosts'
-
-$CONFIGURATION_VENV_BIN_PATH/ansible-playbook -i $HOSTS_FILE -e@$SERVER_VARS $CONFIGURATION_REPO_PATH/playbooks/edx-update-certs.yml
-
--- update_insights_edx2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CONFIGURATION_VENV_BIN_PATH='/edx/app/edx_ansible/venvs/edx_ansible/bin'
-CONFIGURATION_REPO_PATH='/edx/app/edx_ansible/edx_ansible'
-SERVER_VARS='/edx/app/edx_ansible/server-vars.yml'
-HOSTS_FILE='/edx/app/edx_ansible/hosts'
-
-cd $CONFIGURATION_REPO_PATH/playbooks
-$CONFIGURATION_VENV_BIN_PATH/ansible-playbook -i $HOSTS_FILE -e@$SERVER_VARS $CONFIGURATION_REPO_PATH/playbooks/edx-update-insights.yml -e "edx_hosts=edx2"
